29e4356197310fb443585db5735de83de5dae6d3
hiimkobeand <hiimkobeand@gmail.com>
2026-01-03 13:07:39 +0100

    feat: WIP

--- tracker/src/lib.rs
+++ tracker/src/lib.rs
@@ -1,10 +1,19 @@
 mod models;
-use models;
-use reqwest::Client;
+use keyring::{Entry, mock};
+use reqwest::{Client, Method};
+
+use crate::models::TimeEntry;
 
 pub fn get_current_time_entry() -> Option<TimeEntry> {
-    let client = Client::new().
-    let resp = reqwest::get("https://api.track.toggl.com/api/v9/me/time_entries/current")
+    set_default_store(mock::Store::new().expect("Could not set default store"));
+    let client = Client::new();
+    let resp = client.request(
+        Method::GET,
+        "https://api.track.toggl.com/api/v9/me/time_entries/current",
+    );
 }
 
-
+pub fn set_api_key(key: String) {
+    let entry = Entry::new("cosmic-ext-time-tracker", "toggl-api-key");
+    entry.set_secret()
+}
